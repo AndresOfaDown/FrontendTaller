0bda2d0ccb1a0fd011400409e8ef0452bc9fcadd
Luberth08 <luberthgutierrez@gmail.com>
2026-04-26 21:07:40 -0400

    Clean repository without secrets

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+// ng build --configuration=production
+export const environment = {
+  production: true,
+  apiUrl: 'https://backend-repo-2ncr.onrender.com/api/v1'
+  // apiUrl: 'http://localhost:8000/api/v1'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+//ng serve
+export const environment = {
+  production: false,
+  apiUrl: 'https://backend-repo-2ncr.onrender.com/api/v1'
+  // apiUrl: 'http://localhost:8000/api/v1'
+};
